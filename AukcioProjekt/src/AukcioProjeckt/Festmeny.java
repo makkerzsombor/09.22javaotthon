f0a89ef041750792173ef28740d573fa5cd29acf
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-22 08:21:50 +0200

    Festmeny methods done

--- AukcioProjekt/src/AukcioProjeckt/Festmeny.java
+++ AukcioProjekt/src/AukcioProjeckt/Festmeny.java
@@ -16,4 +16,40 @@ public class Festmeny {
         this.festo = festo;
         this.stilus = stilus;
     }
+
+    public String getFesto() {
+        return festo;
+    }
+
+    public String getStilus() {
+        return stilus;
+    }
+
+    public int getLicitekSzama() {
+        return licitekSzama;
+    }
+
+    public int getLegmagasabbLicit() {
+        return legmagasabbLicit;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje() {
+        return legutolsoLicitIdeje;
+    }
+
+    public boolean isEltelt() {
+        return eltelt;
+    }
+
+    public void setElkelt(boolean elketlt){
+
+    }
+
+    public void licit(){
+
+    }
+
+    public void licit(int mertek){
+
+    }
 }
